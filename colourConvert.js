234dcb39aa258bf72e9ccbe138c4cfe72416dff9
Joe Rawnsley <joe.rawnsley@gmail.com>
2024-11-10 22:06:58 +0000

    written tests for extension feature - hsl input converter. tests all failing currently

--- colourConvert.js
+++ colourConvert.js
@@ -1,26 +1,29 @@
 function rgb(r, g, b) {
+	const isValidArg = (arg) => typeof arg === 'number';
 
-	const isValidArg = (arg) => typeof arg === "number"
-
-	if(!isValidArg(r) || !isValidArg(g) || !isValidArg(b)) {
-		console.log(arguments, "valid rgb value not provided")
-		throw new Error("valid rgb value not provided")
+	if (!isValidArg(r) || !isValidArg(g) || !isValidArg(b)) {
+		console.log(arguments, 'valid rgb value not provided');
+		throw new Error('valid rgb value not provided');
 	}
 
 	const toHex = (value) => {
-		if(value > 255) {
-			value = 255
+		if (value > 255) {
+			value = 255;
 		}
-		if(value < 0) {
-			value = 0
+		if (value < 0) {
+			value = 0;
 		}
 		return value.toString(16).toUpperCase().padStart(2, 0);
-	}
+	};
 
 	const fullHexCode = toHex(r) + toHex(g) + toHex(b);
-	console.log(arguments, fullHexCode)
+	console.log(arguments, fullHexCode);
 
 	return fullHexCode;
 }
 
-module.exports = rgb
+function hsl(h, s, l) {
+	return 'hsl input converter activated';
+}
+
+module.exports = { rgb, hsl };
--- colourConvert.test.js
+++ colourConvert.test.js
@@ -1,68 +1,143 @@
-const rgb = require('./colourConvert');
+const { rgb, hsl } = require('./colourConvert');
 
-describe("rgb input converter", () => {
+describe('rgb input converter', () => {
 	it('returns a string', () => {
 		const result = rgb(100, 0, 255);
 		expect(typeof result).toBe('string');
 	});
-	
-	it("returns a string with a length of 6 characters", () => {
-		const result = rgb(100, 0, 255)
-		expect(result.length).toBe(6)
-	})
-	
-	it("returns a correct conversion from rgb to hex colour 1", () => {
-		const result = rgb(100, 0, 255)
-		expect(result).toBe("6400FF")
-	})
-	
-	it("returns a correct conversion from rgb to hex colour 2", () => {
-		const result = rgb(200, 76, 0)
-		expect(result).toBe("C84C00")
-	})
-	
-	it("returns a correct conversion from rgb to hex colour 3", () => {
-		const result = rgb(67, 183, 104)
-		expect(result).toBe("43B768")
-	})
-	
-	it("returns a correct conversion from rgb to hex colour 4", () => {
-		const result = rgb(187, 45, 230)
-		expect(result).toBe("BB2DE6")
-	})
-	
-	it("returns a correct conversion from rgb to hex colour 5", () => {
-		const result = rgb(0, 0, 0)
-		expect(result).toBe("000000")
-	})
-	
-	it("rounds numbers to closest value in range 0-255 (test 1)", () => {
-		const result = rgb(187, 45, 300)
-		expect(result).toBe("BB2DFF")
-	})
-	
-	it("rounds numbers to closest value in range 0-255 (test 2 including negative numbers)", () => {
-		const result = rgb(-78, 270, 230)
-		expect(result).toBe("00FFE6")
-	})
-	
-	
-	it("throws an error if input values are strings", () => {
-		expect(() => {rgb("187", 45, 230)}).toThrow()
-	})
-	
-	it("throws an error when no arguments are provided", () => {
-		expect(() => {rgb()}).toThrow()
-	})
-	
-	it("throws an error when not enough arguments are provided", () => {
-		expect(() => {rgb(45, 230)}).toThrow()
-	})
-	
-	it("throws an error when arguents are inside an array", () => {
-		expect(() => {rgb([45, 300, 0])}).toThrow()
-	})
-	
-})
 
+	it('returns a string with a length of 6 characters', () => {
+		const result = rgb(100, 0, 255);
+		expect(result.length).toBe(6);
+	});
 
+	it('returns a correct conversion from rgb to hex colour 1', () => {
+		const result = rgb(100, 0, 255);
+		expect(result).toBe('6400FF');
+	});
+
+	it('returns a correct conversion from rgb to hex colour 2', () => {
+		const result = rgb(200, 76, 0);
+		expect(result).toBe('C84C00');
+	});
+
+	it('returns a correct conversion from rgb to hex colour 3', () => {
+		const result = rgb(67, 183, 104);
+		expect(result).toBe('43B768');
+	});
+
+	it('returns a correct conversion from rgb to hex colour 4', () => {
+		const result = rgb(187, 45, 230);
+		expect(result).toBe('BB2DE6');
+	});
+
+	it('returns a correct conversion from rgb to hex colour 5', () => {
+		const result = rgb(0, 0, 0);
+		expect(result).toBe('000000');
+	});
+
+	it('rounds numbers to closest value in range 0-255 (test 1)', () => {
+		const result = rgb(187, 45, 300);
+		expect(result).toBe('BB2DFF');
+	});
+
+	it('rounds numbers to closest value in range 0-255 (test 2 including negative numbers)', () => {
+		const result = rgb(-78, 270, 230);
+		expect(result).toBe('00FFE6');
+	});
+
+	it('throws an error if input values are strings', () => {
+		expect(() => {
+			rgb('187', 45, 230);
+		}).toThrow();
+	});
+
+	it('throws an error when no arguments are provided', () => {
+		expect(() => {
+			rgb();
+		}).toThrow();
+	});
+
+	it('throws an error when not enough arguments are provided', () => {
+		expect(() => {
+			rgb(45, 230);
+		}).toThrow();
+	});
+
+	it('throws an error when arguents are inside an array', () => {
+		expect(() => {
+			rgb([45, 300, 0]);
+		}).toThrow();
+	});
+});
+
+describe('hsl input converter', () => {
+	it('returns a string', () => {
+		const result = hsl(264, 100, 50);
+		expect(typeof result).toBe('string');
+	});
+
+	it('returns a string with a length of 6 characters', () => {
+		const result = hsl(264, 100, 50);
+		expect(result.length).toBe(6);
+	});
+
+	it('returns a correct conversion from rgb to hex colour 1', () => {
+		const result = hsl(264, 100, 50);
+		expect(result).toBe('6400FF');
+	});
+
+	it('returns a correct conversion from rgb to hex colour 2', () => {
+		const result = hsl(23, 100, 39);
+		expect(result).toBe('C84C00');
+	});
+
+	it('returns a correct conversion from rgb to hex colour 3', () => {
+		const result = hsl(139, 46, 49);
+		expect(result).toBe('43B768');
+	});
+
+	it('returns a correct conversion from rgb to hex colour 4', () => {
+		const result = hsl(286, 79, 54);
+		expect(result).toBe('BB2DE6');
+	});
+
+	it('returns a correct conversion from rgb to hex colour 5', () => {
+		const result = hsl(0, 0, 0);
+		expect(result).toBe('000000');
+	});
+
+	it('rounds numbers to closest value in range 0-360 (hue) and 0-100 (saturation, lightness)(test 1)', () => {
+		const result = hsl(500, 100, 59);
+		expect(result).toBe('FF2E2E');
+	});
+
+	it('rounds numbers to closest value in range 0-360 (hue) and 0-100 (saturation, lightness)(test 2)', () => {
+		const result = hsl(-78, -45, 48);
+		expect(result).toBe('7A7A7A');
+	});
+
+	it('throws an error if input values are strings', () => {
+		expect(() => {
+			hsl('187', 45, 76);
+		}).toThrow();
+	});
+
+	it('throws an error when no arguments are provided', () => {
+		expect(() => {
+			hsl();
+		}).toThrow();
+	});
+
+	it('throws an error when not enough arguments are provided', () => {
+		expect(() => {
+			hsl(45, 29);
+		}).toThrow();
+	});
+
+	it('throws an error when arguents are inside an array', () => {
+		expect(() => {
+			hsl([245, 75, 12]);
+		}).toThrow();
+	});
+});
